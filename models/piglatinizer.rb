53b89de51504f7b295271c3d638b304054f555c4
Jesse Byers <jesse.s.byers@gmail.com>
2019-12-03 16:53:47 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,9 +5,9 @@ class PigLatinizer
     @user_phrase = user_phrase
   end
 
-  def piglatinize(string)
+  def piglatinize(@user_phrase)
     result = []
-    word_array = string.split
+    word_array = @user_phrase.split
     word_array.map do |word|
       letter_array = word.chars
       if letter_array[0].downcase.start_with?('a','e','i','o','u')
